87db77a850bbc017d0e30f4372b2597b3c51484e
morisum <moris@r.sannet.ne.jp>
2026-05-27 17:14:09 +0900

    Initial Commit

---
+++ MyTimer2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTimer2
+//
+//  Created by MsMacM on 2026/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTimer2/MyTimer2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimer2App.swift
+//  MyTimer2
+//
+//  Created by MsMacM on 2026/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MyTimer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
